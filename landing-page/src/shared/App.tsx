fc67d4475d2c371796fccc185c7b3d5622f47e6a
Davi Soares <davisrmcd@gmail.com>
2025-02-06 22:33:18 -0300

    Fixed routes bug

--- landing-page/src/shared/App.tsx
+++ landing-page/src/shared/App.tsx
@@ -3,12 +3,10 @@ import FeatureSection from "../pages/dashboard/FeatureSection"
 import FooterSection from "../pages/dashboard/FooterSection"
 import MainSection from "../pages/dashboard/MainSection"
 import Navbar from "../pages/dashboard/Navbar"
-import { Routes } from "../routes"
 
 const App = () => {
   return (
     <>
-    <Routes />
     <Navbar />
     <div className="max-w-7xl mx-auto pt-20 px-6">
     <MainSection />
